286583e16a35bd622599ddea96cf26f97d90a95c
Jaga <jagblst@gmail.com>
2023-11-28 14:27:21 +0200

    Course project
    Outputting a List of Recipes with ngFor

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent implements OnInit {
       'This is simply a test',
       'https://cdn.pixabay.com/photo/2017/10/13/19/00/potato-casserole-2848605_1280.jpg'
     ),
+    new Recipe(
+      'Test recipe 2',
+      'This is simply a test 2',
+      'https://media.istockphoto.com/id/519518676/uk/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D0%BC%D0%B0%D0%B6%D0%B5%D0%BD%D1%96-%D1%8F%D0%B9%D1%86%D1%8F-%D1%96-%D0%BF%D1%96%D0%B4%D1%81%D0%BC%D0%B0%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9-%D1%85%D0%BB%D1%96%D0%B1.jpg?s=612x612&w=is&k=20&c=QutZr7daRjWAmsJTttJmJYkRc4cHI1X36BOgYTNCIKo='
+    ),
   ];
 
   constructor() {}
