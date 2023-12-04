5dbf7e1ace170fa5459d5d36bdd7ddf643fa1416
Jaga <jagblst@gmail.com>
2023-12-04 14:09:03 +0200

    Components & Databinding Deep Dive
    Getting Access to the Template & DOM with @ViewChild

--- components-and-databinding/src/app/cockpit/cockpit.component.ts
+++ components-and-databinding/src/app/cockpit/cockpit.component.ts
@@ -1,11 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrl: './cockpit.component.css',
 })
-export class CockpitComponent {
+export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
@@ -15,19 +22,24 @@ export class CockpitComponent {
     serverContent: string;
   }>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
+
+  constructor() {}
+  ngOnInit(): void {}
 
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
