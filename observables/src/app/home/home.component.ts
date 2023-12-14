ca1f9e7254dc7a9bdd13d83cae0c29fb0b863a9b
Jaga <jagblst@gmail.com>
2023-12-14 12:03:55 +0200

    Observables
    Core of Observables

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstoObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.firstoObsSubscription = interval(1000).subscribe((count) => {
+      console.log(count);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.firstoObsSubscription.unsubscribe();
+  }
 }
