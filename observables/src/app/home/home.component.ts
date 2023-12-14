02ad3cd74a01ff17d21060a82171d864054a16a4
Jaga <jagblst@gmail.com>
2023-12-14 12:43:36 +0200

    Observables
    Errors & Completion

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -21,12 +21,27 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        }
         count++;
       }, 1000);
     });
 
-    this.firstoObsSubscription = customIntervalObservable.subscribe((data) =>
-      console.log(data)
+    this.firstoObsSubscription = customIntervalObservable.subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
+      }
     );
   }
 
