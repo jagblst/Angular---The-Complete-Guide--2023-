46e47386e7aa5d1f0f76eec9d70e7857730be769
Jaga <jagblst@gmail.com>
2023-12-21 17:02:28 +0200

    HTTP Requests
    Observing Different Types of Responses

--- http/src/app/post.service.ts
+++ http/src/app/post.service.ts
@@ -1,7 +1,12 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpEventType,
+  HttpHeaders,
+  HttpParams,
+} from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
 
 @Injectable()
@@ -15,7 +20,8 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         "https://learning-angular-http-re-833b1-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
-        postData
+        postData,
+        { observe: "response" }
       )
       .subscribe(
         (responseData) => {
@@ -55,8 +61,18 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete(
-      "https://learning-angular-http-re-833b1-default-rtdb.europe-west1.firebasedatabase.app/posts.json"
-    );
+    return this.http
+      .delete(
+        "https://learning-angular-http-re-833b1-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+        { observe: "events" }
+      )
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
