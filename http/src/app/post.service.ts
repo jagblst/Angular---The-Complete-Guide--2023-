89483acfe8d5238095333f72955170f24214989a
Jaga <jagblst@gmail.com>
2023-12-21 14:26:50 +0200

    HTTP Requests
    Using a Service for Http Requests

---
+++ http/src/app/post.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Post } from "./post.model";
+import { map } from "rxjs/operators";
+
+@Injectable()
+export class PostsService {
+  constructor(private http: HttpClient) {}
+
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
+        "https://learning-angular-http-re-833b1-default-rtdb.europe-west1.firebasedatabase.app/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
+  }
+
+  fetchPosts() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        "https://learning-angular-http-re-833b1-default-rtdb.europe-west1.firebasedatabase.app/posts.json"
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key))
+              postsArray.push({ ...responseData[key], id: key });
+          }
+          return postsArray;
+        })
+      )
+      .subscribe((posts) => {});
+  }
+}
