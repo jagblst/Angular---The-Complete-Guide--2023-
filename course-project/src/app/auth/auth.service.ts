08b03172b05f4c9ccb6bf9817f55ced9193157b2
Jaga <jagblst@gmail.com>
2023-12-26 16:41:09 +0200

    Course project
    Preparing the Signup Request

---
+++ course-project/src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+interface AuthresponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthresponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDrYhf6mg9DD6SEDvtyecwiqXoJzF5vL_c',
+      { email: email, password: password, returnSecureToken: true }
+    );
+  }
+}
